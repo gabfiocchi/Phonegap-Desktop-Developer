3fe2e410b5829f01bef4c4eb6161ba4bb8cd8fdc
gabfiocchi <gabrielfiocchi@hotmail.com>
2014-10-30 16:04:32 -0300

    Phonegap Desktop Developer
    Phonegap Desktop Developer Mac OS en Español

---
+++ PhoneGap.app/Contents/Resources/app.nw/js/drag-n-drop-handlers.js
@@ -0,0 +1,25 @@
+function handleDragOver(evt) {
+    evt.stopPropagation();
+    evt.preventDefault();
+    evt.originalEvent.dataTransfer.dropEffect = 'copy'; // Explicitly show this is a copy.
+} 
+
+function handleDrop(evt) {   
+    evt.stopPropagation();
+    evt.preventDefault();
+
+    var length = evt.originalEvent.dataTransfer.items.length;
+    for (var i = 0; i < length; i++) {
+        var entry = evt.originalEvent.dataTransfer.items[i].webkitGetAsEntry();
+        if (entry.isFile) {
+            console.log("archivo");
+            // if user drags a file, put them into the normal add / open project workflow
+            displayAddCreateProjectOverlay();
+        } else if (entry.isDirectory) {
+            console.log("carpeta: " + entry.fullPath);
+            // prompt user into normal add / open project workflow - we may want to consider automatically adding the folder as a project but we would
+            // need to add logic to determine if the folder contains a valid project. (validProject) ? openExisting : createNew
+            displayAddCreateProjectOverlay();
+        }
+    }
+}
---
+++ PhoneGap.app/Contents/Resources/app.nw/js/utils.js
@@ -0,0 +1,43 @@
+function isEmptyField(value) {
+    var isEmpty = true;
+    
+    if (value){
+        if (value.length > 0) {
+            isEmpty = false;
+        }        
+    }
+       
+    return isEmpty;
+}
+
+function isProjectPathFieldEmpty(value) {
+    var isEmpty = true;
+    var prompt = "Por favor, elija una ruta local";
+    
+    if (value != prompt) {
+        isEmpty = false;
+    }
+    
+    return isEmpty;
+}
+
+function sortByProperty(property) {
+    'use strict';
+    return function (a, b) {
+        var sortStatus = 0;
+        if (a[property] < b[property]) {
+            sortStatus = -1;
+        } else if (a[property] > b[property]) {
+            sortStatus = 1;
+        }
+        return sortStatus;
+    }
+}
+
+function openIssueTracker() {
+    gui.Shell.openExternal("https://github.com/hermwong/phonegap-gui/issues?state=open");    // opens user's default browser & loads page 
+}
+
+function openTutorials() {
+    gui.Shell.openExternal("https://github.com/hermwong/phonegap-gui/wiki/PhoneGap-Developer-Desktop-App-Overview");    // opens user's default browser & loads page 
+}
